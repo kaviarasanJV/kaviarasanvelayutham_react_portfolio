1b54eaee32eb5207a9e7117f2566ee43aafe6e87
kavi <jvandco30@gmail.com>
2025-10-08 21:49:45 +0530

    created the portfolio components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-
-function App() {
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import MyWork from './Components/MyWork/MyWork'
+import Contact from './Components/Contact/Contact'
+const App = () => {
   return (
-    <div className="App">
-      TESTING
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <MyWork/>
+      <Contact/>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
